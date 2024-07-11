7b6f27abf3c6a07c7e744c1f52b571e5b10c6234
Rafael Alvarez <rafaelalvarez3@proton.me>
2024-07-10 19:18:43 -0500

    Creating the periodic table of elements. First commit.

--- Elements/ContentView.swift
+++ Elements/ContentView.swift
@@ -10,10 +10,14 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "hand.wave")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+                .font(.largeTitle)
+                .padding()
+            Text("Hello, Friend!")
+                .font(.title)
+                .padding()
         }
         .padding()
     }
