fce367237265d23e7689afd4ef581811de5927b4
Rafael Alvarez <rafaelalvarez3@proton.me>
2024-07-13 11:46:13 -0500

    Experimenting with the grid/table of the elements.

--- Elements/Views/PeriodicElementTable.swift
+++ Elements/Views/PeriodicElementTable.swift
@@ -10,18 +10,36 @@ import SwiftUI
 struct PeriodicElementTable: View {
     @Environment(ModelData.self) var modelData
     
+    
     var body: some View {
         VStack {
-            HStack {
-                ForEach(modelData.periodicElements) { element in
-                    PeriodicElementCell(periodicElement: element)
+            //HStack {
+                //ForEach(modelData.periodicElements) { element in
+                    //PeriodicElementCell(periodicElement: element)
+                //}
+            //}
+            
+            Grid {
+                ForEach(0..<7) { _ in
+                    GridRow {
+                        ForEach(0..<18) { _ in
+                            RoundedRectangle(cornerRadius: 10)
+                                .fill(.gray)
+                                .frame(width: 75, height: 75)
+                        }
+                    }
                 }
             }
+            
         }
     }
 }
 
 #Preview {
-    PeriodicElementTable()
-        .environment(ModelData())
+    let periodicElements = ModelData().periodicElements
+    return Group {
+        PeriodicElementTable()
+            .environment(ModelData())
+    }
+    
 }
