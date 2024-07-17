65e47e635c1fd08352ca30dcad6eafe2fcd3935d
Rafael Alvarez <rafaelalvarez3@proton.me>
2024-07-17 10:13:36 -0500

    Made the table and list views visible on the ContentView view.

--- Elements/ContentView.swift
+++ Elements/ContentView.swift
@@ -9,8 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        PeriodicElementTable()
-            .padding()
+        VStack {
+            PeriodicElementTable()
+                .padding()
+            PeriodicElementList()
+        }
     }
 }
 
